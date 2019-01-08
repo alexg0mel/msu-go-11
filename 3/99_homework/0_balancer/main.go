177ba213db8856963bca85fae8fcd321a44d67a1
Abramtsev Alexandre <alexgomel@tut.by>
2019-01-08 23:53:04 +0300

    and balancer witch channel

--- 3/99_homework/0_balancer/main.go
+++ 3/99_homework/0_balancer/main.go
@@ -1,18 +1,20 @@
 package main
 
-import "sync"
-
 type RoundRobinBalancer struct {
-	sync.Mutex
-	nodes []int
-	count int
+	ch     chan bool
+	worker chan int
+	nodes  []int
+	count  int
 }
 
 func (r *RoundRobinBalancer) Init(size int) {
+	r.ch = make(chan bool)
+	r.worker = make(chan int)
 	for i := 0; i < size; i++ {
 		r.nodes = append(r.nodes, 0)
 	}
 	r.count = 0
+	go r.do()
 }
 
 func (r *RoundRobinBalancer) GiveStat() []int {
@@ -21,15 +23,24 @@ func (r *RoundRobinBalancer) GiveStat() []int {
 }
 
 func (r *RoundRobinBalancer) GiveNode() int {
-	r.Lock()
-	defer r.Unlock()
-	for index, item := range r.nodes {
-		if item == r.count {
-			r.nodes[index]++
-			return index
+	r.ch <- true
+	return <-r.worker
+}
+
+func (r *RoundRobinBalancer) do() {
+start:
+	for {
+		<-r.ch
+		for index, item := range r.nodes {
+			if item == r.count {
+				r.nodes[index]++
+				r.worker <- index
+				continue start
+			}
 		}
+		r.count++
+		r.nodes[0]++
+		r.worker <- 0
 	}
-	r.count++
-	r.nodes[0]++
-	return 0
+
 }
